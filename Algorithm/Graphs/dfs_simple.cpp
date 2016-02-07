e500da27c0e428068280d832999100775d570fd5
Raman Jaiswal <vipram91@gmail.com>
2016-02-07 14:28:47 +0530

    [DFS] SEGmentation fault Fixed.

--- Algorithm/Graphs/dfs_simple.cpp
+++ Algorithm/Graphs/dfs_simple.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <stdlib.h>
-
+#include<stdio.h>
 using namespace std;
 
 struct Node{
@@ -26,23 +26,28 @@ class Graph{
 
 Graph::Graph(int num){
 	num_nodes = num;
-	node_list = (struct Node_list *)malloc(sizeof(struct Node *)*num);
+	node_list = (struct Node_list *)malloc(sizeof(struct Node_list)*num);
 	for(int i=0;i<num;i++)
-		node_list[i].head_node->next_node=NULL;
+		node_list[i].head_node=NULL;
 }
 
 void Graph::insert_edges(int src, int dest){
 	struct Node *new_node = (struct Node *)malloc(sizeof(Node));
 	new_node->dest_node = dest;
 	new_node->next_node = NULL;
-	if(node_list[src].head_node->next_node == NULL)
-		node_list[src].head_node->next_node = new_node;
-	else{
+	printf("Debug before insert %d to %d \n",src,dest);
+
+	if(node_list[src].head_node == NULL)
+		{
+		printf(" Head src= %d  dest=%d \n",src,dest);
+		node_list[src].head_node = new_node;
+}	else{
 		struct Node *temp;
 		temp = node_list[src].head_node;
-		while(temp->next_node->next_node != NULL){
+		while(temp->next_node != NULL){
 			temp = temp->next_node;
-		}
+				}
+		printf("Not Immediate temp->dest_node =%d src= %d  dest=%d \n",temp->dest_node,src,dest);
 		temp->next_node = new_node;
 	}
 }
@@ -50,9 +55,12 @@ void Graph::insert_edges(int src, int dest){
 void Graph::print_adj_graph(){
 	for(int i=0;i<num_nodes;i++){
 		cout<<endl;
-		int j = 0;
-		while(node_list[i].head_node[j].next_node != NULL)
-			cout<<"\nedge from node "<<j<<" to "<<node_list[i].head_node[j].dest_node;
+		struct Node *temp=node_list[i].head_node;
+		while(temp )
+			{
+			cout<<"\nedge from node "<<i<<" to "<<temp->dest_node;
+			temp=temp->next_node;
+			}
 	}
 }
 int main(){
