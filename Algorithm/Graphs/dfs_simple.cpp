cb6b8c09255401cb7d27b45094f9a1b4697a8330
hackmonster <mrsharma.sid@gmail.com>
2016-02-07 12:55:07 +0530

    [Graph completed]
    Adjucency list implementation

--- Algorithm/Graphs/dfs_simple.cpp
+++ Algorithm/Graphs/dfs_simple.cpp
@@ -26,46 +26,74 @@ class Graph{
 
 Graph::Graph(int num){
 	num_nodes = num;
-	node_list = (struct Node_list *)malloc(sizeof(struct Node *)*num);
-	for(int i=0;i<num;i++)
-		node_list[i].head_node->next_node=NULL;
+	node_list = (struct Node_list *)malloc(sizeof(struct Node_list )*num);
+	//cout<<"init1"<<endl;
+	for(int i=0;i<num;i++){
+		//cout<<"init[i] : "<<i<<endl;
+		node_list[i].head_node=NULL;
+	}
+	//cout<<"init done"<<endl;
 }
 
 void Graph::insert_edges(int src, int dest){
-	struct Node *new_node = (struct Node *)malloc(sizeof(Node));
+	//cout<<"\nIn insert_edges";
+	struct Node *new_node = (struct Node *)malloc(sizeof(Node ));
+	//cout<<"\nMalloc success";
 	new_node->dest_node = dest;
+	//cout<<"\nSet dest value";
 	new_node->next_node = NULL;
-	if(node_list[src].head_node->next_node == NULL)
-		node_list[src].head_node->next_node = new_node;
+	//cout<<"\nInsert start";
+	if(node_list[src].head_node==NULL){
+		//cout<<"head[i] : "<<src<<" is null";
+		node_list[src].head_node = new_node;
+		cout<<"\nInserted node "<<dest<<" at "<<src;
+	}
 	else{
 		struct Node *temp;
 		temp = node_list[src].head_node;
-		while(temp->next_node->next_node != NULL){
+		while(temp->next_node != NULL){
 			temp = temp->next_node;
 		}
+		
 		temp->next_node = new_node;
+		cout<<"\nInserted node "<<dest<<" at "<<src;
 	}
 }
 
 void Graph::print_adj_graph(){
 	for(int i=0;i<num_nodes;i++){
-		cout<<endl;
-		int j = 0;
-		while(node_list[i].head_node[j].next_node != NULL)
-			cout<<"\nedge from node "<<j<<" to "<<node_list[i].head_node[j].dest_node;
+		struct Node *temp;
+		temp = node_list[i].head_node;
+		cout<<"\n Edge from "; 
+		for(int j = 0;temp!=NULL;j++){
+			cout<<"\t"<<i<<" to "<<temp->dest_node;
+			temp = temp->next_node;
+		}
 	}
 }
 int main(){
+	//cout<<"0";
 	Graph g(5);
+	//cout<<"1";
 	g.insert_edges(0,4);		
+	//cout<<"2";
 	g.insert_edges(1,3);		
+	//cout<<"3";
 	g.insert_edges(2,1);		
+	//cout<<"4";
 	g.insert_edges(3,3);		
+	//cout<<"5";
 	g.insert_edges(3,2);		
+	//cout<<"6";
 	g.insert_edges(4,3);		
+	//cout<<"7";
 	g.insert_edges(3,0);		
+	//cout<<"8";
 	g.insert_edges(2,0);		
+	//cout<<"9";
 	g.insert_edges(1,4);
+	//cout<<"10";
 	g.print_adj_graph();		
+	cout<<endl;
 	return 0;
 }
